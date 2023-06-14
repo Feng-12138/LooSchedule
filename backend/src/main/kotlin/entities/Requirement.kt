1c7e4ce53a1fc442eb95e70972fc4dffaf7be794
DV4ABY <yzhan666@gmail.com>
2023-06-13 22:51:21 -0400

    Updated requirement entity

--- backend/src/main/kotlin/entities/Requirement.kt
+++ backend/src/main/kotlin/entities/Requirement.kt
@@ -15,26 +15,26 @@ typealias CourseList = String       // List of required courses for the degree
 typealias RequirementNotes = String // Any additional notes in degree requirements
 typealias URL = String              // Link to undergrad calendar for accurate information
 
-//@Entity
-//@Table(name = "Requirement")
-//class Requirement {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Column(name = "requirementID", nullable = false)
-//    var requirementID: Long? = null
-//
-//    @Column(name = "type", nullable = false)
-//    var type: DegreeType? = null
-//
-//    @Column(name = "year", nullable = false)
-//    var year: Year? = null
-//
-//    @Column(name = "courses")
-//    var courses: CourseList? = null
-//
-//    @Column(name = "additionalRequirements")
-//    var additionalRequirements: RequirementNotes? = null
-//
-//    @Column(name = "link")
-//    var link: URL? = null
-//}
+@Entity
+@Table(name = "Requirement")
+class Requirement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "requirementID", nullable = false)
+    var requirementID: Long? = null
+
+    @Column(name = "type", nullable = false)
+    var type: DegreeType? = null
+
+    @Column(name = "year", nullable = false)
+    var year: Year? = null
+
+    @Column(name = "courses")
+    var courses: CourseList? = null
+
+    @Column(name = "additionalRequirements")
+    var additionalRequirements: RequirementNotes? = null
+
+    @Column(name = "link")
+    var link: URL? = null
+}
