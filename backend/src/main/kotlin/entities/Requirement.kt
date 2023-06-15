8e4cd2c322959b31079476d95d367be4eae4b044
DV4ABY <yzhan666@gmail.com>
2023-06-15 08:56:02 -0400

    Added mapping to entities

--- backend/src/main/kotlin/entities/Requirement.kt
+++ backend/src/main/kotlin/entities/Requirement.kt
@@ -1,40 +1,4 @@
 package entities
 
-import javax.persistence.*
-
-enum class DegreeType {
-    MAJOR,
-    MINOR,
-    SPECIALIZATION,
-    Joint,
-    DIPLOMA,
-    OPTION
-}
-typealias Year = String             // Year which the degree plan is for
-typealias CourseList = String       // List of required courses for the degree
-typealias RequirementNotes = String // Any additional notes in degree requirements
-typealias URL = String              // Link to undergrad calendar for accurate information
-
-@Entity
-@Table(name = "Requirement")
 class Requirement {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "requirementID", nullable = false)
-    var requirementID: Long? = null
-
-    @Column(name = "type", nullable = false)
-    var type: DegreeType? = null
-
-    @Column(name = "year", nullable = false)
-    var year: Year? = null
-
-    @Column(name = "courses")
-    var courses: CourseList? = null
-
-    @Column(name = "additionalRequirements")
-    var additionalRequirements: RequirementNotes? = null
-
-    @Column(name = "link")
-    var link: URL? = null
 }
