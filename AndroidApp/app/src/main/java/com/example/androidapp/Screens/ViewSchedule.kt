b6868e9fc5b09c70eca0b917e76842a36da6a692
StevenTianRa <steventian20@gmail.com>
2023-06-15 22:02:58 -0400

    skeleton shedule

--- AndroidApp/app/src/main/java/com/example/androidapp/Screens/ViewSchedule.kt
+++ AndroidApp/app/src/main/java/com/example/androidapp/Screens/ViewSchedule.kt
@@ -1,5 +1,6 @@
 package com.example.androidapp.Screens
 
+import android.graphics.fonts.Font
 import android.widget.Button
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.horizontalScroll
@@ -11,21 +12,33 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.androidapp.Screen
 
@@ -36,9 +49,13 @@ val courseList = listOf("CS 135", "MATH 135", "MATH 137", "EMLS 129R", "PHYS 111
 @Composable
 private fun TermButton(term : String) {
     Surface(
-        modifier = Modifier.padding(horizontal = 10.dp)
+        modifier = Modifier
+            .padding(horizontal = 10.dp)
+            .height(50.dp)
+            .width(100.dp)
     ) {
-        Button(onClick = { /*TODO*/ }) {
+        Button(onClick = { /*TODO*/ },
+        ) {
             Text(text = term)
         }
     }
@@ -49,10 +66,17 @@ private fun CourseDescription(course : String) {
     Surface(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 10.dp)
+            .padding(horizontal = 10.dp, vertical = 10.dp)
+            .height(150.dp)
     ) {
-        TextButton(onClick = { /*TODO*/ }) {
-            Text(text = course)
+        TextButton(onClick = { /*TODO*/ },
+            shape = RoundedCornerShape(20.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.LightGray)) {
+            Text(text = course, color = Color.Black, fontSize = 25.sp,
+            textAlign = TextAlign.Left)
+
+//            Text(text = "Description", textAlign = TextAlign.Right)
         }
     }
 }
@@ -64,12 +88,14 @@ fun ViewSchedule(navController: NavController){
     ) {
         Column(modifier = Modifier.padding(10.dp)){
             Row(modifier = Modifier
-                .padding(0.dp)
+                .padding(vertical = 10.dp)
                 .horizontalScroll(rememberScrollState())) {
                 for (term in termList) {
                     TermButton(term = term)
                 }
             }
+
+            Spacer(modifier = Modifier.size(20.dp))
 
             // Course schedule for the each term
             Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
