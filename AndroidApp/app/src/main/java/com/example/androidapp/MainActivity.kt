df2fc8421baab2d7f091f43df8b44b0ab1566787
Fakevin0613 <ke_qinyang@outlook.com>
2023-06-24 16:10:52 -0400

    repair mainActivity in android

--- AndroidApp/app/src/main/java/com/example/androidapp/MainActivity.kt
+++ AndroidApp/app/src/main/java/com/example/androidapp/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.androidapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
 
 
 class MainActivity : ComponentActivity() {
