5698aa3bb53a224c3bdd78714307553fb9622503
StevenTianRa <steventian20@gmail.com>
2023-07-01 21:08:51 -0400

    add pager for view schedule

--- AndroidApp/app/src/main/java/com/example/androidapp/Screens/ViewSchedule.kt
+++ AndroidApp/app/src/main/java/com/example/androidapp/Screens/ViewSchedule.kt
@@ -1,9 +1,11 @@
 package com.example.androidapp.screens
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -12,26 +14,36 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.androidapp.models.Course
 import com.example.androidapp.viewModels.ScheduleViewModel
+import kotlinx.coroutines.launch
+import java.lang.Integer.min
 
 @Composable
 private fun CourseDescription(course: Course, navController: NavController) {
@@ -58,7 +70,21 @@ private fun CourseDescription(course: Course, navController: NavController) {
                 Text(
                     text = course.courseID,
                     Modifier.align(Alignment.TopStart),
-                    color = Color.Black, fontSize = 25.sp
+                    style = MaterialTheme.typography.displaySmall,
+                    color = Color.Black
+                )
+                Text(
+                    text = course.courseName,
+                    Modifier.align(Alignment.CenterStart),
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = Color.Black
+                )
+                Text(
+                    text = course.description,
+                    Modifier.align(Alignment.BottomStart),
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 2,
+                    color = Color.Black
                 )
             }
         }
@@ -66,40 +92,73 @@ private fun CourseDescription(course: Course, navController: NavController) {
 }
 
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ViewSchedule(navController: NavController, scheduleViewModel: ScheduleViewModel){
     var currentTerm by remember { mutableStateOf(scheduleViewModel.currentTerm) }
-    var selectedTabIndex by remember { mutableStateOf(scheduleViewModel.selectedTabIndex) }
-    var listState : LazyListState = rememberLazyListState()
+    var pagerPage by remember { mutableStateOf(scheduleViewModel.selectedTabIndex) }
+    var termList = scheduleViewModel.termList
+    val pagerState = rememberPagerState()
+    val scope = rememberCoroutineScope()
+    val pagerCourses = remember { mutableStateListOf<List<Course>>() }
+
+    LaunchedEffect(key1 = scheduleViewModel.selectedTabIndex) {
+        pagerPage = scheduleViewModel.selectedTabIndex
+    }
 
     Surface(
-        modifier = Modifier.padding(horizontal = 10.dp)
+        modifier = Modifier
+            .padding(horizontal = 10.dp)
+            .fillMaxSize()
     ) {
         Column(modifier = Modifier.padding(horizontal = 10.dp)) {
             ScrollableTabRow(
-                selectedTabIndex = selectedTabIndex,
+                selectedTabIndex = min(termList.count(), pagerPage),
                 edgePadding = 0.dp
             ) {
-                scheduleViewModel.schedule.keys.forEachIndexed { index, term ->
+                scheduleViewModel.schedule.keys.forEachIndexed { index, tabName ->
                     Tab(
-                        selected = index == selectedTabIndex,
-                        onClick = { scheduleViewModel.onTermSelected(term)
+                        selected = index == pagerPage,
+                        onClick = { scheduleViewModel.onTermSelected(tabName)
                                     currentTerm = scheduleViewModel.currentTerm
-                                    selectedTabIndex = scheduleViewModel.selectedTabIndex },
-                        text = { Text(term) }
+                                    pagerPage = scheduleViewModel.selectedTabIndex
+                                    scope.launch { pagerState.animateScrollToPage(index) } },
+                        text = { Text(tabName) }
                     )
                 }
             }
 
             Spacer(modifier = Modifier.size(10.dp))
 
-            LazyColumn(
-                state = listState
-            ) {
-                items(scheduleViewModel.schedule[currentTerm]!!) { course ->
-                    CourseDescription(course = course, navController = navController)
+            HorizontalPager(
+                pageCount = termList.count(),
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxSize(),
+                state = pagerState,
+                verticalAlignment = Alignment.Top
+            ) { page ->
+                val courses = pagerCourses.getOrElse(page) {
+                    val term = termList[page]
+                    val courses = scheduleViewModel.schedule[term] ?: emptyList()
+                    pagerCourses.add(page, courses)
+                    courses
                 }
+                CourseSchedulePage(courses = courses, navController = navController)
             }
+        }
+    }
+}
+
+@Composable
+private fun CourseSchedulePage(courses: List<Course>, navController: NavController) {
+    if (courses.isEmpty()) return
+
+    LazyColumn(
+        state = rememberLazyListState()
+    ) {
+        items(courses) { course ->
+            CourseDescription(course = course, navController = navController)
         }
     }
 }
