5c4a498b2b8317b64a253afd80d13f848423ec9b
Fakevin0613 <ke_qinyang@outlook.com>
2023-07-22 17:05:24 -0400

    dynamic list of degree

--- AndroidApp/app/src/main/java/com/example/androidapp/EverythingManager.kt
+++ AndroidApp/app/src/main/java/com/example/androidapp/EverythingManager.kt
@@ -4,7 +4,7 @@ import com.example.androidapp.dataClass.Everything
 
 class EverythingManager private constructor() {
     // Data fields that you want to share
-    private var someData: Everything? = null
+    private var everything: Everything? = null
 
     companion object {
         // The single instance of DataManager
@@ -19,10 +19,40 @@ class EverythingManager private constructor() {
     }
 
     fun setEverything(data: Everything) {
-        someData = data
+        everything = data
     }
 
-    fun getEverything(): Everything? {
-        return someData
+    fun getCourses(): List<String>? {
+        return everything?.courses
+    }
+
+    fun getMajors(): List<String> {
+        return mutableListOf("Select your degree").apply {
+            everything?.majors.let {
+                if (it != null) {
+                    this.addAll(it)
+                }
+            }
+        }
+    }
+
+    fun getMinors(): List<String> {
+        return mutableListOf("Select your minor").apply {
+            everything?.minors.let {
+                if (it != null) {
+                    this.addAll(it)
+                }
+            }
+        }
+    }
+
+    fun getSpecializations(): List<String> {
+        return mutableListOf("Select your specialization").apply {
+            everything?.specializations.let {
+                if (it != null) {
+                    this.addAll(it)
+                }
+            }
+        }
     }
 }
--- AndroidApp/app/src/main/java/com/example/androidapp/Navigation.kt
+++ AndroidApp/app/src/main/java/com/example/androidapp/Navigation.kt
@@ -29,62 +29,6 @@ import com.google.gson.Gson
 fun Navigation(){
     val navController = rememberNavController()
     val selectDegreeVM = SelectDegreeVM(LocalContext.current, navController = navController)
-//    val courseList1A = listOf(
-//        Course("CS 135"), Course("MATH 135"),
-//        Course("MATH 137"), Course("EMLS 129R"), Course("PHYS 111")
-//    )
-//    val courseList1B = listOf(
-//        Course("1B 1"), Course("1B 2"), Course("1B 3"),
-//        Course("1B 4"), Course("1B 5")
-//    )
-//    val courseList2A = listOf(
-//        Course("2A 1"), Course("2A 2"), Course("2A 3"),
-//        Course("2A 4"), Course("2A 5")
-//    )
-//    val courseList2B = listOf(
-//        Course("2B 1"), Course("2B 2"), Course("2B 3"),
-//        Course("2B 4"), Course("2B 5")
-//    )
-//    val courseList3A = listOf(
-//        Course("3A 1"), Course("3A 2"), Course("3A 3"),
-//        Course("3A 4"), Course("3A 5")
-//    )
-//    val courseList3B = listOf(
-//        Course("3B 1"), Course("3B 2"), Course("3B 3"),
-//        Course("3B 4"), Course("3B 5")
-//    )
-//    val courseList4A = listOf(
-//        Course("4A 1"), Course("4A 2"), Course("4A 3"),
-//        Course("4A 4"), Course("4A 5")
-//    )
-//    val courseList4B = listOf(
-//        Course("4B 1"), Course("4B 2"), Course("4B 3"),
-//        Course("4B 4"), Course("4B 5")
-//    )
-//
-//    val test = Schedule(mapOf("1A" to courseList1A, "1B" to courseList1B,
-//        "2A" to courseList2A, "2B" to courseList2B,
-//        "3A" to courseList3A, "3B" to courseList3B,
-//        "4A" to courseList4A, "4B" to courseList4B))
-//
-//    val test2 = Schedule(mapOf("1A" to courseList1A, "1B" to courseList1B,
-//        "2A" to courseList2A, "2B" to courseList2B,
-//        "3A" to courseList3A, "3B" to courseList3B,
-//        "4A" to courseList4A))
-
-
-//    var testList: List<Schedule> = listOf(test, test2)
-
-//    val sharedPreferences = LocalContext.current.getSharedPreferences("MySchedules", Context.MODE_PRIVATE)
-//
-//    val editor = sharedPreferences.edit()
-//    val jsonList = testList.map { Gson().toJson(it) }.toSet()
-//    editor.putStringSet("scheduleList", jsonList)
-//    editor.apply()
-//
-//    val existingList = sharedPreferences.getStringSet("scheduleList", emptySet())?.toList()
-//    val scheduleList = existingList?.map { Gson().fromJson(it, Schedule::class.java) } ?: emptyList()
-//    val scheduleViewModel = ScheduleViewModel(scheduleList[0])
 
     NavHost(navController = navController, startDestination = Screen.MainScreen.route){
         composable(route = Screen.About.route){
