9255036e66b9235aa9952825b54c776626b41143
Fakevin0613 <ke_qinyang@outlook.com>
2023-07-23 14:02:50 -0400

    remove dotenv

--- data/scraper/courses/__init__.py
+++ data/scraper/courses/__init__.py
@@ -1,13 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,scoped_session
 from sqlalchemy.ext.declarative import declarative_base
-from dotenv import dotenv_values, load_dotenv
 import os
 
-load_dotenv()
 # Change the .env file to your personal one
-config = dotenv_values(".env")
-pathToDB = os.environ['pathToDB']
+# config = dotenv_values(".env")
+# please use your own path
+pathToDB = "/Users/keqinyang/Desktop/CS446/LooSchedule/data/db/LooSchedule.db"
 engine = create_engine(f'sqlite:///{pathToDB}')
 Session = scoped_session(
     sessionmaker(bind=engine)
