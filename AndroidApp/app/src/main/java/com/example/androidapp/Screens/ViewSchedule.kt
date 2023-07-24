56840623e4b7e642c7bc0dc345b0a6e9346ec57a
Fakevin0613 <ke_qinyang@outlook.com>
2023-07-24 18:55:23 -0400

    validate button

--- AndroidApp/app/src/main/java/com/example/androidapp/Screens/ViewSchedule.kt
+++ AndroidApp/app/src/main/java/com/example/androidapp/Screens/ViewSchedule.kt
@@ -1,43 +1,28 @@
 package com.example.androidapp.screens
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.ThumbUp
-import androidx.compose.material.icons.filled.Warning
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.Surface
@@ -46,7 +31,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -58,7 +42,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.androidapp.models.Course
 import com.example.androidapp.viewModels.ScheduleViewModel
@@ -174,7 +157,39 @@ fun ViewSchedule(navController: NavController, scheduleViewModel: ScheduleViewMo
             }
         }
 
-        AddButton()
+        Box(
+            contentAlignment = Alignment.BottomEnd,
+            modifier = Modifier
+                .padding(vertical = 24.dp, horizontal = 12.dp)
+        ) {
+            Column() {
+                FloatingActionButton(
+                    containerColor = MaterialTheme.colorScheme.secondary,
+                    contentColor = Color.White,
+                    shape = CircleShape,
+                    modifier = Modifier
+                        .padding(bottom = 12.dp),
+                    onClick = { /* Handle button click here */ }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Add"
+                    )
+                }
+
+                FloatingActionButton(
+                    containerColor = MaterialTheme.colorScheme.secondary,
+                    contentColor = Color.White,
+                    shape = CircleShape,
+                    onClick = { /* Handle button click here */ }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Done,
+                        contentDescription = "Validate"
+                    )
+                }
+            }
+        }
     }
 }
 
@@ -217,28 +232,6 @@ private fun CourseSchedulePage(courses: List<Course>, navController: NavControll
             items(courses) { course ->
                 CourseDescription(course = course, navController = navController)
             }
-        }
-    }
-}
-
-// Add course button
-@Composable
-fun AddButton() {
-    Box(
-        contentAlignment = Alignment.BottomEnd,
-        modifier = Modifier
-            .padding(vertical = 16.dp)
-    ) {
-        FloatingActionButton(
-            containerColor = Color.LightGray,
-            contentColor = Color.White,
-            shape = CircleShape,
-            onClick = { /* Handle button click here */ }
-        ) {
-            Icon(
-                imageVector = Icons.Default.Add,
-                contentDescription = "Add"
-            )
         }
     }
 }
